0c7b478b830e17eefb5710ea361d477e4d160738
Vasco Alexandre da Silva Costa <1638760+vcosta@users.noreply.github.com>
2023-02-08 07:42:42 +0000

    add bind with IPv6

--- mood.c
+++ mood.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define MAX_STATE 256
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define MAX_RNG_STATE 256
 
 #define MAX_BUFFER 128
 
@@ -18,10 +22,28 @@ void loop(void) {
 int main(void) {
 /*	unsigned int seed = 66666; */
 	unsigned int seed = 12345;
-	char state[MAX_STATE];
+	char state[MAX_RNG_STATE];
 
-	initstate(seed, state, MAX_STATE);
+	initstate(seed, state, MAX_RNG_STATE);
 	setstate(state);
+
+	int sd;
+	sd = socket(AF_INET6, SOCK_STREAM, 0);	/* PF_INET? */
+
+	struct linger opt;
+	opt.l_onoff = 0;
+	opt.l_linger = 0;
+	setsockopt(sd, SOL_SOCKET, SO_LINGER, &opt, sizeof(opt));
+
+	struct sockaddr_in6 addr;
+	memset(&addr, 0, sizeof(addr));
+	addr.sin6_family = AF_INET6;
+	addr.sin6_port = 1024;
+
+	if (bind(sd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+		perror("bind error: ");
+		exit(1);
+	}
 	
 	for (;;) {
 		loop();
