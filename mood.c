f94a6adeec2c587787659eb5c5774d2339cef776
Vasco Alexandre da Silva Costa <vasco.costa@gmail.com>
2023-02-08 06:20:29 +0000

    Add files via upload
    init

---
+++ mood.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+//	srandom(6666666);
+//	srandom(12345);
+	srandom(time(NULL));
+
+	long mood = (random() % 3);
+
+	printf("you mood is: %s\n", ((mood == 0) ? "fucked" : ((mood == 1) ? "screwed" : "okayish")));
+	return 0;
+}
