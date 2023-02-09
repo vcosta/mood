0d50718e2807ccc14dc05d5a2a3e0ba393f1e94b
Vasco Alexandre da Silva Costa <1638760+vcosta@users.noreply.github.com>
2023-02-09 01:19:04 +0000

    added select call to code.

--- mood.c
+++ mood.c
@@ -5,15 +5,17 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
-
-#include <sys/epoll.h>
+/* #include <sys/epoll.h> */
 
 
 #define MAX_RNG_STATE 256
 
 #define MAX_BUFFER 128
 
-#define MAX_EPOLL_EVENTS 10
+/* #define MAX_EPOLL_EVENTS 10 */
+
+#define MAX(a, b)	((a) > (b) ? (a) : (b))
+
 
 const char *moods[] = {
 	"your mood is: fucked\n",
@@ -26,6 +28,10 @@ const char *infos[] = {
 	"you are down in the dumps\n",
 	"it is raining outside\n"
 };
+
+
+static int max_conns;
+static int *conns;
 
 void doit(int conn_sock) {
 	char buf[MAX_BUFFER];
@@ -75,19 +81,64 @@ int main(void) {
 
 	listen(sd, 10);
 
-	for (;;) {
-		struct sockaddr_in6 conn_addr;
-		socklen_t conn_addrlen;
-		int conn_sock;
+	max_conns = 0;
+	conns = NULL;
 
-		conn_sock = accept(sd, (struct sockaddr *) &conn_addr, &conn_addrlen);
-		if (conn_sock == -1) {
-			perror("accept error: ");
-			exit(1);
+	for (;;) {
+		fd_set in_set, out_set, exc_set;
+		int max_desc;
+
+		FD_ZERO(&in_set);
+		FD_ZERO(&out_set);
+		FD_ZERO(&exc_set);
+
+		FD_SET(sd, &in_set);
+		FD_SET(sd, &exc_set);
+
+		max_desc = sd;
+		for (int i = 0; i < max_conns; i++) {
+			const int conn_sock = conns[i];
+			if (conn_sock != -1) {
+				FD_SET(conn_sock, &out_set);
+				FD_SET(conn_sock, &exc_set);
+				max_desc = MAX(max_desc, conn_sock);
+			}
 		}
 
-		doit(conn_sock);
-		close(conn_sock);
+		select(max_desc+1, &in_set, &out_set, &exc_set, NULL);
+
+		if (FD_ISSET(sd, &in_set)) {
+			struct sockaddr_in6 conn_addr;
+			socklen_t conn_addrlen;
+			int conn_sock;
+
+			conn_sock = accept(sd, (struct sockaddr *) &conn_addr, &conn_addrlen);
+			if (conn_sock == -1) {
+				perror("accept error: ");
+				exit(1);
+			}
+			conns = realloc(conns, (max_conns+1) * sizeof(*conns));
+			conns[max_conns] = conn_sock;
+			max_conns++;
+		}
+
+		for (int i = 0; i < max_conns; i++) {
+			const int conn_sock = conns[i];
+			if (FD_ISSET(conn_sock, &exc_set)) {
+				close(conn_sock);
+				max_conns--;
+				conns[i] = -1;
+			}
+
+			if (FD_ISSET(conn_sock, &out_set)) {
+				doit(conn_sock);
+				close(conn_sock);
+				max_conns--;
+				conns[i] = -1;
+			}
+		}
+
+		/* DEFRAG conns */
 	}
 
 	return 0;
